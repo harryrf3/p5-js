e608ca1d140669eff43c14956b3ab15ac796fe14
Harry Fahringer III <harryrf3.dev@gmail.com>
2022-02-21 11:23:01 -0500

    A perlin noise loop made in 3d space

--- perlin-noise_js-inheritance/p5/src/sketch.p5.js
+++ perlin-noise_js-inheritance/p5/src/sketch.p5.js
@@ -1,32 +1,44 @@
 /**
- * Perlin Noise Loops and JS Inheritance
+ * Perlin Noise Loops
  * CodingTrain https://youtu.be/7k-iJyHq7-k
- * (resume: 24:45)
  *-
  * @harryrf3 (2022-02-21T00:16:46.000-05:00)
  ***/
 
-
-function setup() {
-  createCanvas(600, 600);
-}
-
-function draw() {
-  background(22);
-  // center in screen
-  translate(width / 2, height / 2);
-  // line color
-  stroke(255);
-  // remove fill color
-  noFill();
-  beginShape();
-
-  for (let a = 0; a < TWO_PI; a += 0.01) {
-    let r = 100;
-    let x = r * cos(a);
-    let y = r * sin(a);
-    vertex(x, y);
-  }
-
-  endShape();
-}
+ let noiseMax = 0.5;
+ let slider;
+ let phase = 0;
+ let zoff = 0; 
+ 
+ function setup() {
+   createCanvas(600, 600);
+   slider = createSlider(0, 10, 0, 0.1);
+ }
+ 
+ function draw() {
+   // sets noiseSeed to a fixed number
+   // noiseSeed(99); // can be randomized
+   background(0, 25);
+   // center in screen
+   translate(width / 2, height / 2);
+   // line color
+   stroke(255);
+   // remove fill color
+   noFill();
+   beginShape();
+   noiseMax = slider.value();
+   for (let a = 0; a < TWO_PI; a += 0.1) {
+     let xoff = map(cos(a + phase), -1, 1, 0, noiseMax);
+     let yoff = map(sin(a + phase), -1, 1, 0, noiseMax);
+     let r = map(noise(xoff, yoff, zoff), 0, 1, 100, 200);
+     let x = r * cos(a);
+     let y = r * sin(a);
+     vertex(x, y);
+   }
+  
+   endShape(CLOSE);
+   zoff += 0.01;
+   phase += 0.01;
+   // noLoop();
+ }
+ 
