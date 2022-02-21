4b7da5742d44281afbb9507b2d2e9990c4d1065a
Harry Fahringer III <harryrf3.dev@gmail.com>
2022-02-21 02:11:13 -0500

    Create a circle on the screen.
    TODO: Continue on with video then add my own variations to the code.

--- perlin-noise_js-inheritance/p5/src/sketch.p5.js
+++ perlin-noise_js-inheritance/p5/src/sketch.p5.js
@@ -1,7 +1,7 @@
 /**
  * Perlin Noise Loops and JS Inheritance
- * 
- * 
+ * CodingTrain https://youtu.be/7k-iJyHq7-k
+ * (resume: 24:45)
  *-
  * @harryrf3 (2022-02-21T00:16:46.000-05:00)
  ***/
@@ -12,5 +12,21 @@ function setup() {
 }
 
 function draw() {
-  background(255);
+  background(22);
+  // center in screen
+  translate(width / 2, height / 2);
+  // line color
+  stroke(255);
+  // remove fill color
+  noFill();
+  beginShape();
+
+  for (let a = 0; a < TWO_PI; a += 0.01) {
+    let r = 100;
+    let x = r * cos(a);
+    let y = r * sin(a);
+    vertex(x, y);
+  }
+
+  endShape();
 }
