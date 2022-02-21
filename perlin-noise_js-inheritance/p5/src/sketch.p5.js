084184960e516f8c1923c76ae96b42f463b787f6
Harry Fahringer III <harryrf3.dev@gmail.com>
2022-02-21 00:46:43 -0500

    Created p5.js base app

---
+++ perlin-noise_js-inheritance/p5/src/sketch.p5.js
@@ -0,0 +1,16 @@
+/**
+ * Perlin Noise Loops and JS Inheritance
+ * 
+ * 
+ *-
+ * @harryrf3 (2022-02-21T00:16:46.000-05:00)
+ ***/
+
+
+function setup() {
+  createCanvas(600, 600);
+}
+
+function draw() {
+  
+}
